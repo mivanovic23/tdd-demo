86bbfb37335636cdbd7c5a272dacebd524edd543
Milorad Ivanović <45767180+mivanovic23@users.noreply.github.com>
2020-04-15 15:45:57 +0200

    Update fizzBuzz.js

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,8 @@
 function fizzBuzz(x) {
     let result;
-    if(x%3 == 0) result = "Fizz"
     if(x%5 == 0) result = "Buzz"
-    if(x%3 == 0 && x%5 == 0) result = "FizzBuzz";
+    if(x%3 == 0) result = "Fizz"
+    if(x%5 == 0 && x%3 == 0) result = "FizzBuzz";
     
     return result;
     
